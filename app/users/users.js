27a86e5d0697d203700218a0cdc64dbefb7c331f
Sean Nelson <smnelsonn@gmail.com>
2019-08-24 16:35:20 -0700

    get request from my server, working in users

--- app/users/users.js
+++ app/users/users.js
@@ -7,8 +7,16 @@ angular.module('momentousApp.users', ['ngRoute'])
     templateUrl: 'users/users.html',
     controller: 'UsersCtrl'
   });
+
 }])
 
-.controller('UsersCtrl', [function() {
-  
-}]);
+.controller('UsersCtrl', function($scope, $http) {
+  $scope.formData = {};
+  $http.get('http://localhost:8888/users')
+      .then(function(data) {
+          $scope.users = data;
+          console.log(data);
+      }, function(error) {
+          console.log('Error: ' + data);
+      });
+});
