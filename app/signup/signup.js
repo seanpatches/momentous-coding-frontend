61b298b742bdce0b99626c86fa3a0096fce64e30
Sean Nelson <smnelsonn@gmail.com>
2019-08-26 12:17:52 -0700

    successfully creating user on DB

--- app/signup/signup.js
+++ app/signup/signup.js
@@ -12,7 +12,14 @@ signupModule.controller('SignupCtrl', function($scope, $http) {
   console.log('got to right controller')
   $scope.formData = {};
   $scope.signupSubmit = function(){
-    console.log('hi')
+    const {name, email, bio} = $scope.formData;
+    console.log(name, email)
+    $http.post('http://localhost:8888/users', $scope.formData)
+      .then(function(res){
+        console.log(res.data)
+      }), function(error){
+        console.log('Error: ' + data)
+      }
   }
   window.MY_SCOPE = $scope;
 });
