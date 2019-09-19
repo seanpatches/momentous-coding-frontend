e43a146f6e692b93051100ac0c25561d3de9ab9c
Sean Nelson <smnelsonn@gmail.com>
2019-09-19 13:36:25 -0700

    properly searching userList to put in local storage

--- app/callback/callback.controller.js
+++ app/callback/callback.controller.js
@@ -6,20 +6,30 @@
     .module('app')
     .controller('CallbackController', callbackController);
 
-  function callbackController() {}
-  //see if userId from Auth0 is already in the database
-  const currentUserId = localStorage.getItem('userId');
-  $http.get('http://localhost:8888/users')
-      .then(userList => {
-        //check userList for a corresponding authID, if so obtain userDB info
-      }, error => {
-          console.log('Error: ' + data);
-      });
-  //if the authId is not associated with a dbUser, create a user in DB    
-  // $http.post('http://localhost:8888/users')
-  //     .then(function(data) {
-  //         $scope.users = data;
-  //     }, function(error) {
-  //         console.log('Error: ' + data);
-  //     });
+  function callbackController($http) {
+    //see if userId from Auth0 is already in the database
+    const currentUserId = localStorage.getItem('userId');
+    $http.get('http://localhost:8888/users')
+        .then(userList => {
+          userList.data.forEach(user=> {
+            if(currentUserId === user.authId){
+              localStorage.setItem('user', user);
+              return;
+            }
+          })
+        })
+        .then(userList => {
+          //if user doesn't exist, post one
+        })
+        , error => {
+            console.log('Error: ' + data);
+        };
+    //if the authId is not associated with a dbUser, create a user in DB    
+    // $http.post('http://localhost:8888/users')
+    //     .then(function(data) {
+    //         $scope.users = data;
+    //     }, function(error) {
+    //         console.log('Error: ' + data);
+    //     });
+  }
 })();
