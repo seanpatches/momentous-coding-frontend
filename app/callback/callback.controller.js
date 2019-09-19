36557481e44c1f99305e148c53c3b8bca5b5d70d
Sean Nelson <smnelsonn@gmail.com>
2019-09-19 13:13:56 -0700

    psuedocoding and structuring auth0 user tied to db user

--- app/callback/callback.controller.js
+++ app/callback/callback.controller.js
@@ -7,5 +7,19 @@
     .controller('CallbackController', callbackController);
 
   function callbackController() {}
-
+  //see if userId from Auth0 is already in the database
+  const currentUserId = localStorage.getItem('userId');
+  $http.get('http://localhost:8888/users')
+      .then(userList => {
+        //check userList for a corresponding authID, if so obtain userDB info
+      }, error => {
+          console.log('Error: ' + data);
+      });
+  //if the authId is not associated with a dbUser, create a user in DB    
+  // $http.post('http://localhost:8888/users')
+  //     .then(function(data) {
+  //         $scope.users = data;
+  //     }, function(error) {
+  //         console.log('Error: ' + data);
+  //     });
 })();
--- app/users/allUsers.controller.js
+++ app/users/allUsers.controller.js
@@ -8,9 +8,9 @@ angular
 function allUsersController($scope, $http) {
   $scope.formData = {};
   $http.get('http://localhost:8888/users')
-      .then(function(data) {
+      .then(data => {
           $scope.users = data;
-      }, function(error) {
+      }, error => {
           console.log('Error: ' + data);
       });
   window.MY_SCOPE = $scope;
